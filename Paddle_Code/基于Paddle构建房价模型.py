034c0b82a78613e466a9a418cae60d00d63dd176
sanbai <2845556715@qq.com>
2024-10-27 15:02:59 +0800

    Paddle实验1

---
+++ Paddle_Code/基于Paddle构建房价模型.py
@@ -0,0 +1,28 @@
+"""
+    1.加载库文件
+    2.数据预处理
+        2.1.加载数据集
+        2.2.数据形状变换
+        2.3.数据集划分
+        2.4.数据归一化
+        2.5.封装函数
+    3.搭建神经网络
+    4.配置训练模型
+    5.模型训练
+    6.测试训练模型
+"""
+import paddle
+import paddle.fluid as fluid
+import paddle.fluid.dygraph as dygraph
+from paddle.fluid.dygraph import Linear
+import numpy as np
+import os,random
+
+def load_data():
+    #2.1 加载数据集
+    datafile = "datasets/boston.data"
+    data = np.fromfile(datafile,sep=" ")
+    feature_names = ["CRIM","ZN","INDUS","CHAS","NOX","RM","RM","AGE","DIS","RAD","TAX","B","LSTAT","MEDV"]
+    feature_num = len(feature_names)
+    #2.2数据形状转换
+    data = data.reshape([data.shape[0]//feature_num,feature_num])
