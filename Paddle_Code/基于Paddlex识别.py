964bd2cf7fb9ff33bf456cc10567421a1a0ce08b
sanbai <2845556715@qq.com>
2024-11-02 16:16:12 +0800

    表格识别实现

---
+++ Paddle_Code/基于Paddlex识别.py
@@ -0,0 +1,8 @@
+from paddlex import create_pipeline
+pipeline = create_pipeline(pipeline="table_recognition")
+output = pipeline.predict("table_recognition.jpg")
+for res in output:
+    # res.print() ## 打印预测的结构化输出
+    res.save_to_img("D:/Python/img") ## 保存img格式结果
+    res.save_to_xlsx("D:/Python/img") ## 保存表格格式结果
+    res.save_to_html("D:/Python/img") ## 保存html结果
