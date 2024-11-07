284c22c392975c16aad5bf97d2e7c4b96bdc31d0
sanbai <2845556715@qq.com>
2024-11-07 18:05:04 +0800

    PyQt学习

--- pyqt_note.py
+++ pyqt_note.py
@@ -1,6 +1,6 @@
 import sys
 
-from PyQt5.QtWidgets import QApplication,QWidget
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QLineEdit,QDesktopWidget
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -8,7 +8,33 @@ if __name__ == '__main__':
     w = QWidget()
     
     w.setWindowTitle("第一个PyQt")#设置窗口标题
+    w.resize(1500,800)
+    # 创建按钮
+    # btn = QPushButton("按钮")
+    # btn.setParent(w)# 添加到窗口显示
+
+    # 创建纯文本
+    label = QLabel("账号:",w)# 创建一个(纯文本)，创建时指定父对象
+    label.setGeometry(200,200,300,200)# 显示位置与大小:x,y,w,h
+
+    # 创建文本框
+    edit =QLineEdit(w)
+    edit.setPlaceholderText("请输入账号")
+    edit.setGeometry(550,200,200,20)
+
+    # 窗口设置在屏幕左上角
+    w.move(0,0)
     
-    w.show()# 展示窗口
-    
+    # 调整窗口在屏幕中央显示
+    center_pointer = QDesktopWidget().availableGeometry().center()
+    x = center_pointer.x()
+    y = center_pointer.y()
+    w.move(x,y)
+    print(w.frameGeometry())
+    print(w.frameGeometry().getRect())
+    print(type(w.frameGeometry().getRect()))
+    old_x,old_y,width,height = w.frameGeometry().getRect()
+    w.move(x - width/2,y - height/2)
+
+    w.show()# 展示窗口 
     app.exec_()#程序进行循环等待状态
