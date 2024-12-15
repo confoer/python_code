eab4954affc193b463b91552642575db051aeda3
sanbai <2845556715@qq.com>
2024-12-15 08:29:56 +0800

    语义分割调试

--- Paddle_Code/语义分割.py
+++ Paddle_Code/语义分割.py
@@ -1,9 +1,7 @@
-from paddlex import create_pipeline
-
-pipeline = create_pipeline(pipeline="semantic_segmentation")
-
-output = pipeline.predict("makassaridn-road_demo.png")
+from paddlex import create_model
+model = create_model("PP-LiteSeg-T")
+output = model.predict("img\\makassaridn-road_demo.png", batch_size=1)
 for res in output:
-    res.print() ## 打印预测的结构化输出
-    res.save_to_img("./output/") ## 保存结果可视化图像
-    res.save_to_json("./output/") ## 保存预测的结构化输出
+    res.print(json_format=False)
+    res.save_to_img("./output_test/")
+    res.save_to_json("./output_test/res.json")
